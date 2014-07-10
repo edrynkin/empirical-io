e410d849ceb83d2beec73a6c2e2e0f2031f24da7
Marat Salikhov <marat.salikhov@gmail.com>
2014-07-10 15:40:21 +0400

    Berry (1994) -- improved comments
    No implementation changes.

--- berry1994/implementation.py
+++ berry1994/implementation.py
@@ -7,14 +7,14 @@ def berry_inversion(hats, tildes):
     Berry inversion step: given sample market shares, estimate deltas
     Arguments:
     hats (ndarray, N x 1): market shares in sample
-    tildes (function R^n -> R^n): a mapping from deltas to market shares
+    tildes (function N x 1 -> N x 1): a mapping from deltas to market shares
 
     Returns:
-    delta_hat (ndarray, N x 1): estimated deltas
+    delta_hat (ndarray, N x 1): estimated deltas (mean utility vector)
     """
-    tildes_minus_hats = lambda delta: tildes(delta) - hats
-    delta_0 = np.zeros_like(hats)
-    delta_hat = fsolve(tildes_minus_hats,delta_0)
+    tildes_minus_hats = lambda delta: tildes(delta) - hats # s(d) = S <=> s(d) - S = 0
+    delta_0 = np.zeros_like(hats) # initial approximation
+    delta_hat = fsolve(tildes_minus_hats,delta_0) # solving for the root
     return delta_hat
 
 
@@ -25,6 +25,7 @@ def iv_regression(X,Z,p,delta_hat):
     X (ndarray, N x K) -- demand
     Z (ndarray, N x I) -- instruments
     p (ndarray, N x 1) -- prices
+    delta_hat (nd_array, N x 1) -- estimated mean utility vector
 
     Returns:
     alpha (double): coefficient on prices (p)
@@ -50,7 +51,7 @@ def berry_estimator(X, Z, p, s_hat, s_tilde):
 
     Returns:
     alpha (double): coefficient on prices (p)
-    beta (ndarray, N x K): coefficients on demand determinants (X)
+    beta (ndarray, N x K): coefficients on observed demand-determining characteristics(X)
     """
     delta_hat = berry_inversion(s_hat, s_tilde)
     alpha, beta = iv_regression(X, Z, p, delta_hat)
@@ -69,3 +70,6 @@ def logit_shares(delta):
     exp_delta = np.exp(delta)
     sum_exp_delta = np.sum(exp_delta) + 1
     return exp_delta / sum_exp_delta
+
+def vertical_differentiation():
+    pass
