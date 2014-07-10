83d02ede2974ef4dfa86bfde3df6cb4881499dee
Marat Salikhov <marat.salikhov@gmail.com>
2014-07-10 22:44:33 +0400

    Berry (1994) -- vertical differentiation
    Added vertical differentiation model specification
    * Berry inversion step for this specification
    * Some tests (not exhaustive though)
    
    Hope I'll be able to come up with a feasible test data generator for this model.

--- berry1994/implementation.py
+++ berry1994/implementation.py
@@ -25,7 +25,7 @@ def iv_regression(X,Z,p,delta_hat):
     X (ndarray, N x K) -- demand
     Z (ndarray, N x I) -- instruments
     p (ndarray, N x 1) -- prices
-    delta_hat (nd_array, N x 1) -- estimated mean utility vector
+    delta_hat (ndarray, N x 1) -- estimated mean utility vector
 
     Returns:
     alpha (double): coefficient on prices (p)
@@ -35,6 +35,9 @@ def iv_regression(X,Z,p,delta_hat):
     p_hat = Z.dot(eta) # fitted values
     X_prime = np.c_[X,p_hat] # appending fitted prices from the 1st stage to the data matrix
     theta,_,_,_ = np.linalg.lstsq(X_prime, delta_hat) # second-stage regression
+    print "delta_hat", delta_hat
+    print "p_hat", p_hat
+    print "theta", theta
     beta, alpha = np.split(theta, [np.size(theta)-1]) # split the estimated coefficient vector ([1..N-1],[N])
     return alpha, beta
 
@@ -60,7 +63,7 @@ def berry_estimator(X, Z, p, s_hat, s_tilde):
 
 def logit_shares(delta):
     """
-    Given delta, estimate market shares according to multinomial logit
+    Given delta, estimate market shares according to multinomial logit model
     Arguments:
     delta (ndarray, N x 1): mean utility vector
 
@@ -71,5 +74,93 @@ def logit_shares(delta):
     sum_exp_delta = np.sum(exp_delta) + 1
     return exp_delta / sum_exp_delta
 
-def vertical_differentiation():
-    pass
+def vd_d_to_shares(d, cdf):
+    """
+    Auxiliary function: convert cut-off points to shares
+    Arguments:
+    d(ndarray, N x 1): cut-off points
+
+    Returns:
+    shares(ndarray, N x 1): market shares
+    """
+    dF = cdf(d)
+    dF = np.append(dF, [1.0])
+    result = np.diff(dF)
+    return result
+
+
+def vd_shares_to_d(s, cdf, i_cdf):
+    """
+    Auxiliary function: convert shares to cut-off points
+
+    Arguments:
+    s (ndarray, N x 1): market shares
+
+    Returns:
+    d (ndarray, N x 1): cut-off points
+    """
+    d = np.zeros(s.size)
+    d[-1] = i_cdf(1 - s[-1])
+    for j in xrange(d.size-2, -1, -1):
+        d[j] = i_cdf(cdf(d[j+1])-s[j])
+    return d
+
+def berry_inversion_vd(shares, prices, cdf, i_cdf):
+    '''
+    Berry inversion step for the Shaked-Sutton vertical differentiation model (section 4, example 2)
+    Arguments:
+    shares (ndarray, N x 1): market shares
+    prices (ndarray, N x 1): product prices
+    cdf (function, double -> double): cdf of the taste parameter (v) for the consumers
+    icdf (function, double -> double): inverse cdf of the taste parameter (v) for the consumers
+
+    Returns:
+    delta(ndarray, N x 1): mean utility levels vector
+    '''
+    d = vd_shares_to_d(shares, cdf, i_cdf)
+    dp = np.diff(np.insert(prices, 0, 0))
+    psi = np.divide(dp,d)
+    psi = np.cumsum(psi)
+    delta = psi - prices
+    return delta
+
+
+def shares_vd(delta, prices, cdf):
+    '''
+    Given delta, prices, and taste parameter cdf, compute shares.
+    Vertical differentiation model is used to specify demand (section 4, example 2).
+    Arguments:
+    delta (ndarray, N x 1): mean utility levels vector
+    prices (ndarray, N x 1): product prices vector
+    cdf (function, double -> double): cdf of the taste parameter (v) for the consumers
+
+    Returns:
+    shares (ndarray, N x 1): vector of computed market shares
+    '''
+    dp = np.diff(np.insert(prices,0,0))
+    psi = delta+prices
+    dps = np.diff(np.insert(psi, 0, 0))
+    d = np.divide(dp,dps)
+    s_hat = vd_d_to_shares(d, cdf)
+
+    return s_hat
+
+
+def berry_estimator_vd(X, Z, p, shares, cdf, i_cdf):
+    """
+    Given X,Z,p,cdf,i_cdf estimate alpha, beta by using Berry's procedure.
+
+    Arguments:
+    X (ndarray, N x K) -- demand determinants (observed characteristics)
+    Z (ndarray, N x I) -- instruments
+    p (ndarray, N x 1) -- prices
+    cdf (function, double -> double): cdf of the taste parameter (v) for the consumers
+    icdf (function, double -> double): inverse cdf of the taste parameter (v) for the consumers
+
+    Returns:
+    alpha (double): coefficient on prices (p)
+    beta (ndarray, N x K): coefficients on observed demand-determining characteristics(X)
+    """
+    delta_hat = berry_inversion_vd(shares, p, cdf, i_cdf)
+    alpha, beta = iv_regression(X, Z, p, delta_hat)
+    return alpha, beta
---
+++ berry1994/test_vertical_differentiation.py
@@ -0,0 +1,33 @@
+import unittest
+import numpy as np
+import scipy.stats
+from berry1994.implementation import berry_inversion_vd, shares_vd, berry_estimator_vd, vd_d_to_shares, vd_shares_to_d
+
+
+class TestVerticalDifferentiation(unittest.TestCase):
+
+
+    def setUp(self):
+        self.N = 10
+        self.beta = 15
+        self.cdf  = lambda x: scipy.stats.uniform.cdf(x,scale=1,loc=0.5)
+        self.icdf = lambda x: scipy.stats.uniform.ppf(x,scale=1,loc=0.5)
+        self.X, self.Z, self.p, self.s_hat, self.delta = \
+            self.generate_data(self.N, self.beta, self.cdf)
+
+    def test_cdf(self):
+        self.assertEqual(0.5,self.cdf(self.icdf(0.5)))
+
+    def test_d_to_s(self):
+        s = np.array([0.1, 0.2, 0.3, 0.4])
+        d = vd_shares_to_d(s,self.cdf,self.icdf)
+        spp = vd_d_to_shares(d,self.cdf)
+        self.assertTrue(np.allclose(s,spp))
+
+    def test_berry_inversion(self):
+        shares = np.array([0.1, 0.2, 0.3, 0.4])
+        prices = np.array([2.0, 5.0, 6.0, 8.0])
+        delta_hat = berry_inversion_vd(shares, prices, self.cdf, self.icdf)
+        s_hat = shares_vd(delta_hat,prices, self.cdf)
+        correct = np.allclose(shares,s_hat)
+        self.assertTrue(correct)
