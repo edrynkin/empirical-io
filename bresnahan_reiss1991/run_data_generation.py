347138c39f4e19a37ad304ce731013058b1d2fa4
Evgeni Drynkin <e.drynkin@gmail.com>
2014-07-26 15:47:04 +0400

    consistency check

--- bresnahan_reiss1991/run_data_generation.py
+++ bresnahan_reiss1991/run_data_generation.py
@@ -5,13 +5,13 @@ from data_generation import *
 import numpy as np
 
 np.random.seed(6)
-alpha = np.array([5.0, 0.25, 0.75])
+alpha = np.array([10.0, 0.25, 0.75])
 beta = np.array([1.0, 2.0, 5.0, 7.0])
 gamma = np.array([1.0, 2.0, 3.0])
 delta = np.array([1.0, 2.0])
 kappa = np.array([1.0, 7.0, 8.0])
 actual_params = [np.log(alpha), beta, np.log(gamma), delta, kappa]
-M = 400
+M = 4000
 N = alpha.size
 n, W, X, Y, Z = generate_data(alpha, beta, gamma, delta, kappa, M)
 theta_0 = combine_theta(np.log(alpha), beta, np.log(gamma), delta, kappa)
